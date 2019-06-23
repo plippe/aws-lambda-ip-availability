b6dc994847ae655b45483bd5c4082b7f4b6798c2
Philippe Vinchon <p.vinchon@gmail.com>
2019-06-23 21:40:45 +0100

    Move metric data to cloudwatch

--- src/aws/cloudwatch.js
+++ src/aws/cloudwatch.js
@@ -7,6 +7,43 @@ const putMetricData = params => {
   });
 };
 
+const metricData = (
+  regionName,
+  vpcId,
+  subnetId,
+  totalIpAddress,
+  availableIpAddressCount
+) => {
+  const metricDataDimensions = (regionName, vpcId, subnetId) => {
+    return [
+      { Name: "Region", Value: regionName },
+      { Name: "VpcId", Value: vpcId },
+      { Name: "SubnetId", Value: subnetId }
+    ];
+  };
+
+  const metricData = (name, value, dimensions) => {
+    return { MetricName: name, Value: value, Dimensions: dimensions };
+  };
+
+  return {
+    Namespace: "subnet-ip-availability",
+    MetricData: [
+      metricData(
+        "TotalIpAddress",
+        totalIpAddress,
+        metricDataDimensions(regionName, vpcId, subnetId)
+      ),
+      metricData(
+        "AvailableIpAddress",
+        availableIpAddressCount,
+        metricDataDimensions(regionName, vpcId, subnetId)
+      )
+    ]
+  };
+};
+
 module.exports = {
+  metricData: metricData,
   putMetricData: putMetricData
 };
